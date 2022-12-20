98bd714ebe549b162df6351d217844de0ad1715a
blm <mcleanb@photo-nomad.net>
2022-12-20 06:03:38 -0500

    fix windows path?

--- docs/development/Electronite/meta_builds/set_build_env.bat
+++ docs/development/Electronite/meta_builds/set_build_env.bat
@@ -2,7 +2,8 @@ rem set Environment
 
 set vs2019_install=c:\Program Files (x86)\Microsoft Visual Studio\2019\Community
 set WINDOWSSDKDIR=c:\Program Files (x86)\Windows Kits\10
-set Path=%cd%\depot_tools;%Path%;C:\Program Files\nodejs;C:\python27\Scripts;C:\python27;C:\python311\Scripts;C:\python311;C:\Program Files\Git\cmd
+
+set Path=%cd%\depot_tools;%Path%
 
 call python3 --version
 call python2 --version
